48b592ee72fd99b2e8f4badb51bfcde19aa2e943
Alexey Kostylev <42770259+Zoisait@users.noreply.github.com>
2018-10-02 21:29:11 +0300

    Учебный проект: разделяй и властвуй (#6)
    * Выполнено задание  #18 Учебный проект: в движении
    
    * Выполнено задание #20 Учебный проект: разделяй и властвуй

---
+++ js/util.js
@@ -0,0 +1,24 @@
+'use strict';
+(function () {
+
+  var WIZARDS_NUMBER = 4;
+  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
+  window.util = {
+    WIZARDS_NUMBER: WIZARDS_NUMBER,
+    WIZARD_NAMES: WIZARD_NAMES,
+    WIZARD_SURNAMES: WIZARD_SURNAMES,
+    WIZARD_COAT_COLORS: WIZARD_COAT_COLORS,
+    WIZARD_EYES_COLORS: WIZARD_EYES_COLORS,
+    FIREBALL_COLORS: FIREBALL_COLORS,
+    ESC_KEYCODE: ESC_KEYCODE,
+    ENTER_KEYCODE: ENTER_KEYCODE
+  };
+
+})();
